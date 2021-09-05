7eaca647d96cedb5936488cc9b0b9d10f22b364a
amgona94 <87363422+amgona94@users.noreply.github.com>
2021-09-06 06:44:04 +0900

    Add files via upload

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "e8b1d70d03263c156a2f3ae5c79a0298"
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+            city.innerText = data.name;
+        });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
